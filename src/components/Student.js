a81c4d4476895e382a54f08287e763eb68386228
Marco Kwan <marcokwan10@gmail.com>
2022-04-12 04:24:52 -0400

    feat: part 4(expandable list)

--- src/components/Student.js
+++ src/components/Student.js
@@ -1,20 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 import "../App.css";
 
 function Student({ studentInfo }) {
 	const { firstName, lastName, email, company, skill, grades, pic } = studentInfo;
+	const [isExpanded, setIsExpanded] = useState(false);
 
 	return (
 		<div className="student">
 			<img src={pic} alt="profile" className="student-profile" />
 			<div className="student-info">
-				<h1>
-					{firstName} {lastName}
-				</h1>
-				<p>Email: {email}</p>
-				<p>Company: {company}</p>
-				<p>Skill: {skill}</p>
-				<p>Average: {grades.reduce((a, b) => Number(a) + Number(b), 0) / grades.length}%</p>
+				<div>
+					<h1>
+						{firstName} {lastName}
+					</h1>
+					<p>Email: {email}</p>
+					<p>Company: {company}</p>
+					<p>Skill: {skill}</p>
+					<p>Average: {grades.reduce((a, b) => Number(a) + Number(b), 0) / grades.length}%</p>
+					{isExpanded && (
+						<div className="grades">
+							{grades.map((grade, idx) => (
+								<>
+									<p key={idx}>
+										Test {idx + 1}: <span>{grade}%</span>
+									</p>
+								</>
+							))}
+						</div>
+					)}
+				</div>
+				{!isExpanded && (
+					<button className="expand" onClick={() => setIsExpanded(true)}>
+						+
+					</button>
+				)}
+				{isExpanded && (
+					<button className="expand" onClick={() => setIsExpanded(false)}>
+						-
+					</button>
+				)}
 			</div>
 		</div>
 	);
